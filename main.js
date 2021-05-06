2479a03ff91446a377fd39a3a97f7a91ab6f8555
DD10654 <75024070+DD10654@users.noreply.github.com>
2021-05-06 15:00:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,47 +1,30 @@
-var mouseEvent = "empty";
+canvas = document.getElementById("mycanvas");
+ctx = canvas.getContext("2d");
+colour = "red";
 
+ctx.beginPath();
+ctx.strokeStyle = colour;
+ctx.lineWidth = 2;
+ctx.arc(200, 200, 40 ,0 , 2*Math.PI);
+ctx.stroke();
 
-    canvas = document.getElementById('mycanvas');
-    ctx = canvas.getContext("2d");
-    
-    color = "black";
-    width_of_line = 2;
-    canvas.addEventListener("mousedown", my_mousedown);
-    
-    function my_mousedown(e)
-    {
+canvas.addEventListener("mousedown", my_mousedown);
 
-        color = document.getElementById("color").value;
-        width_of_line = document.getElementById("width_of_line").value;
-        radius = document.getElementById("radius").value;
-        mouseEvent = "mouseDown";
-    }
+function my_mousedown(e)
+{
 
-    canvas.addEventListener("mousemove", my_mousemove);
-    function my_mousemove(e)
-    {
-        current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
-        current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
 
-        if (mouseEvent == "mouseDown") {
-        console.log("Current position of x and y coordinates = " + "x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
-        ctx.beginPath();
-        ctx.strokeStyle = color;
-        ctx.lineWidth = width_of_line;
-        ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius ,0 , 2 * Math.PI);
-        ctx.stroke();
-        }
+    console.log("X = " + mouse_x + "<br>" + "Y = " + mouse_y);
+    circle(mouse_x, mouse_y);
+}
 
-    }
-
-    canvas.addEventListener("mouseup", my_mouseup);
-    function my_mouseup(e)
-    {
-        mouseEvent = "mouseUP";
-    }
-
-    canvas.addEventListener("mouseleave", my_mouseleave);
-    function my_mouseleave(e)
-    {
-        mouseEvent = "mouseleave";
-    }
+function circle(mouse_x, mouse_y)
+{
+    ctx.beginPath();
+    ctx.strokeStyle = colour;
+    ctx.lineWidth = 2;
+    ctx.arc(mouse_x, mouse_y, 40 ,0 , 2*Math.PI);
+    ctx.stroke();
+}
